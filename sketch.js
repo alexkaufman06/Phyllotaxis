8fa9d5e7bc34292f1cd39d001ffab5c47d089dde
alexkaufman06 <alexkaufman06@gmail.com>
2018-07-06 10:34:05 -0700

    Add slider

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 var n = 0;
 var c = 4; // scaling factor (how much the radius is changing) (2)
-var angle = 137.5; // can change angle 137.3
+var slider;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   angleMode(DEGREES); // comment out for different effect
   colorMode(HSB);
-  background(0); 
+  background(0);
+  slider = createSlider(0, 360, 137.5, .1); // 137.3 for other pattern
+  slider.position(20,20);
 }
 
 function draw() {
+  angle = slider.value();
   var a = n * angle;
   var r = c * sqrt(n);
   var x = r * cos(a) + width / 2 ;
