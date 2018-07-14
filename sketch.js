ad6ba37d2881659c1c352919b167bdfb92a40763
Alex Kaufman <alexkaufman06@gmail.com>
2018-07-13 22:59:01 -0700

    Add scale slider

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var n = 0;
-var c = 4; // scaling factor (how much the radius is changing) (2)
-var slider;
+// var c = 4; // scaling factor (how much the radius is changing) (2)
+var angleSlider;
+var scaleSlider;
 var seeds = [];
 
 function setup() {
@@ -8,21 +9,23 @@ function setup() {
   angleMode(DEGREES); // comment out for different effect
   colorMode(HSB);
   background(0);
-  slider = createSlider(0, 360, 137.5, .1); // 137.3 for other pattern
-  slider.position(20,40);
+  angleSlider = createSlider(0, 360, 137.5, .1); // 137.3 for other pattern
+  angleSlider.position(20,40);
+  scaleSlider = createSlider(0, 15, 4, .1);
+  scaleSlider.position(20, 70);
 }
 
 function draw() {
   // clear(); background(0);  // creates cool pattern 
-  angle = slider.value();
+  angle = angleSlider.value();
   var a = n * angle;
-  var r = c * sqrt(n);
+  var r = scaleSlider.value() * sqrt(n);
   var x = r * cos(a) + width / 2 ;
   var y = r * sin(a) + height / 2;
   fill(n % 256, 255, 255); // can change n to a;
   noStroke(); // comment out for different effect
   ellipse(x, y, 4, 4); // 8, 8
-  rect(window.innerWidth - 135, 10, 120, 10);
+  rect(26, 60, 120, 10);
   fill(0);
   rect(50, 5, 120, 40);
   fill(255);
